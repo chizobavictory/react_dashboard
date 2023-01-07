bc75aaea9db12391a1f1a1040086900a694c3a97
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-07 19:33:24 +0100

    intitialized react components 🧱

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import './sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top"></div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
+import Sidebar from '../../components/sidebar/Sidebar'
 import './home.scss'
+
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Sidebar />
+    </div>
   )
 }
 
