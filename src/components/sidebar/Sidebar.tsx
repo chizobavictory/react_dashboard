5055c74ddb8b4e301adf537b78e99b50daa6274c
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-07 19:40:29 +0100

    intitialized react components 🧱

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,9 +3,26 @@ import './sidebar.scss'
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="top"></div>
-      <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="top">
+        <span className='logo'>reactadmin</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">color options</div>
     </div>
   )
 }
