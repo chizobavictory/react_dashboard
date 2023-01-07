df79e585b1d67b86bb319aa842f941ad8074197c
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-07 21:54:23 +0100

    intitialized react components 🧱

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,16 @@
 import './sidebar.scss'
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 
 const Sidebar = () => {
@@ -11,17 +22,52 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <li>
-            <DashboardIcon />
+            <DashboardIcon className='icon'/>
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon className='icon'/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+          <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+          <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <li>
+          <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+          <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <li>
+          <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+          <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+          <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <li>
+          <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+          <ExitToAppIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
