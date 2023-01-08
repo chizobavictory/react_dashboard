5e162b3dad29bcbffdfc201585497e767ccc116e
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-08 12:10:48 +0100

    featured component 🧩

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,8 +1,15 @@
 import './featured.scss'
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const Featured = () => {
   return (
-    <div className='featured'>Featured</div>
+    <div className='featured'>
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon className='icon' />
+      </div>
+      <div className="bottom"></div>
+    </div>
   )
 }
 
