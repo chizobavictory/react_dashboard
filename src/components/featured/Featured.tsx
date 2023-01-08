4d235ca48e86f6ad889773f2ba90d516e2e22d94
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-08 14:31:11 +0100

    featured component 🧩

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -2,6 +2,8 @@ import './featured.scss'
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
   return (
@@ -11,8 +13,34 @@ const Featured = () => {
         <MoreVertIcon className='icon' />
       </div>
       <div className="bottom">
-        <div className="featuredChat">
-        <CircularProgressbar value={70} text={'70%'} strokeWidth={4}/>
+        <div className="featuredChart">
+        <CircularProgressbar value={70} text={'70%'} strokeWidth={8}/>
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">Previous transaction processing. Last payments may not be included</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize='small'/>
+              <div className="resultAmount">$12.7k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize='small'/>
+              <div className="resultAmount">$34.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize='small'/>
+              <div className="resultAmount">$40.9k</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
