7d4842cc9658421788e02666699d38e6ea778d66
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-08 12:11:07 +0100

    home page tsx 🔵

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import Chart from '../../components/chart/Chart'
+import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widgets/Widget'
@@ -16,7 +18,8 @@ const Home = () => {
           <Widget type='balance'/>
         </div>
         <div className="charts">
-          
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
