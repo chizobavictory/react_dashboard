1030333ac9fa73638d474309502c2d4513644d5e
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-08 08:20:59 +0100

    navbar component 🧩

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,39 @@
 import './navbar.scss'
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"; 
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon'/>
+            <span className='language'>English</span>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/> 
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
