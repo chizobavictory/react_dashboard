88c4dc54783abecaae50730aca9a75e0d5bcf3f4
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-09 22:12:41 +0100

    single page tsx 🔵

--- src/pages/single/Single.tsx
+++ src/pages/single/Single.tsx
@@ -16,7 +16,7 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src="https://mir-s3-cdn-cf.behance.net/user/276/a7d1b545266123.6224667ab06db.jpg"
                 alt=""
                 className="itemImg"
               />
