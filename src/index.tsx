376d44560baa838d99aeda2d801b4a5c13934356
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-10 14:17:07 +0100

    index page tsx 🔵

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { DarkModeContextProvider } from './context/darkModeContext';
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DarkModeContextProvider>
+      <App />
+    </DarkModeContextProvider>
+  </React.StrictMode>,
 );
 
