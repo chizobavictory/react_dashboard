53c221808c494ff5c17a5400f46981394b564fb7
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-10 10:35:50 +0100

    app page tsx 🔵

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formSource";
 
 function App() {
   return (
@@ -16,12 +17,12 @@ function App() {
           <Route path="users">
             <Route index element={<List/>} />
             <Route path=":userId" element={<Single/>} />
-            <Route path="new" element={<New inputs={[{ id: "string", label: "Label", type: "text", placeholder: "Placeholder" }]} title={"Add New User"}/>} />
+            <Route path="new" element={<New inputs={userInputs} title={"Add New User"}/>} />
           </Route>
           <Route path="products">
             <Route index element={<List/>} />
             <Route path=":productId" element={<Single/>} />
-            <Route path="new" element={<New inputs={[]} title={"New Product"}/>} />
+            <Route path="new" element={<New inputs={productInputs} title={"Add New Product"}/>} />
           </Route>
         </Route>
       </Routes>
