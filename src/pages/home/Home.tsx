6eec1407f84a245ffa3de4d229fa9ff2c4e59d45
Chizoba Victory <chizoba.victory1@gmail.com>
2023-01-17 00:00:04 +0100

    navbar styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,22 @@
-import Chart from '../../components/chart/Chart'
-import Featured from '../../components/featured/Featured'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Table from '../../components/table/Table'
-import Widget from '../../components/widgets/Widget'
-import './home.scss'
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Table from "../../components/table/Table";
+import Widget from "../../components/widgets/Widget";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Sidebar />
+    <div className="home">
+      <Sidebar/>
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type='user'/>
-          <Widget type='order'/>
-          <Widget type='earning'/>
-          <Widget type='balance'/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
         <div className="charts">
           <Featured />
@@ -24,11 +24,11 @@ const Home = () => {
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
-        <Table />
+          <Table />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
